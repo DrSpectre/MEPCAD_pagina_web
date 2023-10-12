fb3756a5e34df7dac463458ad5ed8fef17b37c29
Azxma <bluakorvo@protonmail.com>
2023-10-11 21:10:14 -0600

    [MODIFICA] los estados para poder mostrar o cultar el menu de texto y alterar la ifnromacion relacionada

--- js/data_base.js
+++ js/data_base.js
@@ -1,13 +1,13 @@
-const BDD = [
+const Catalogo = [
     {
         id: "nombre",
         texto: "texto largo o corto, mucho o nada",
-        img: "ruta/del/archivo.jpg",
+        img: "https://i.imgur.com/IiuVaMY.jpeg",
     },
     {
         id: "nombre_2",
-        texto: "texto largo o corto, mucho o nada",
-        img: "ruta/del/archivo.jpg",
+        texto: "texto largo o corto, mucho o nada texto largo o corto, mucho o nada texto largo o corto, mucho o nada",
+        img: "https://i.imgur.com/kbKfb53.jpeg",
     },
 ]
 
--- js/programa.js
+++ js/programa.js
@@ -1,22 +1,45 @@
 const texto_lateral = document.querySelector("#texto_lat");
 const imagen_lateral = document.querySelector("#img_lat");
 
+const estados_posibles = {
+    visisble: "texto_visible",
+    oculto: "texto_oculto"
+}
+
+let estado_actual = estados_posibles.oculto
+
 
 
 function clickeado(...argumentos){
     console.log(argumentos)
 
-    texto_lateral.classList.add("texto_lateral_anim")
-    imagen_lateral.classList.add("imagen_lateral_anim")
+    if(estado_actual == estados_posibles.oculto){
+        texto_lateral.classList.add("texto_lateral_anim")
+        imagen_lateral.classList.add("imagen_lateral_anim")
 
-    texto_lateral.querySelector("#min_texto").innerText = "MODIFUCKERS"
-    imagen_lateral.querySelector("img").src = "https://i.imgur.com/IiuVaMY.jpeg"
+        let entrada_encontrada = Catalogo.find((entrada) => entrada.id == argumentos[1])
+        console.log(entrada_encontrada)
+
+        texto_lateral.querySelector("#min_texto").innerText = entrada_encontrada.texto
+        imagen_lateral.querySelector("img").src = entrada_encontrada.img
+
+        estado_actual = estados_posibles.visisble
+    }
+
+    else if(estado_actual == estados_posibles.visisble){
+        texto_lateral.classList.remove("texto_lateral_anim")
+        imagen_lateral.classList.remove("imagen_lateral_anim")
+
+        estado_actual = estados_posibles.oculto
+    }
 }
 
 
 function cerrar_ventanas(...argumentos){
     texto_lateral.classList.remove("texto_lateral_anim")
     imagen_lateral.classList.remove("imagen_lateral_anim")
+
+    estado_actual = estados_posibles.oculto
 }
 
 
