015cfd56a93c953bfec66d36165c6b5c2469f899
Azxma <bluakorvo@protonmail.com>
2023-10-11 20:55:13 -0600

    [AREEGLA] error sobre la ubicacion de elementos

--- js/programa.js
+++ js/programa.js
@@ -4,11 +4,13 @@ const imagen_lateral = document.querySelector("#img_lat");
 
 
 function clickeado(...argumentos){
-    console.log("Hola mundo")
     console.log(argumentos)
 
     texto_lateral.classList.add("texto_lateral_anim")
     imagen_lateral.classList.add("imagen_lateral_anim")
+
+    texto_lateral.querySelector("#min_texto").innerText = "MODIFUCKERS"
+    imagen_lateral.querySelector("img").src = "https://i.imgur.com/IiuVaMY.jpeg"
 }
 
 
