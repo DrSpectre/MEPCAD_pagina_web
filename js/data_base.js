1b2d218951dc7318bb28da2162c355eab764a443
Azxma <bluakorvo@protonmail.com>
2023-10-26 20:54:28 -0600

    [AGREGA] los elementos faltantes actuales de las obras enviadas

--- js/data_base.js
+++ js/data_base.js
@@ -11,6 +11,40 @@ const Catalogo = [
         img: "img/galeria/origins_men_art.JPG",
     },
     {
+        id: "cuadernos_amor_1",
+        texto: {
+            titulo: "arte de las series \"Los Cuadernos del Amor\"",
+            material: "Ilustración",
+            anyo: "2011",
+            autor: "Erick Nungaray",
+            acerca_obra: "Los Cuadernos del Amor fue una serie de dibujos realizada entre el 2010 y 2014 que ilustra irónicamente el amor y el  fracaso.",
+        },
+        img: "img/galeria/cuadernos_amor_1.jpg",
+    },
+    {
+        id: "cuadernos_amor_2",
+        texto: {
+            titulo: "arte de las series \"Los Cuadernos del Amor\"",
+            material: "Ilustración",
+            anyo: "2011",
+            autor: "Erick Nungaray",
+            acerca_obra: "Los Cuadernos del Amor fue una serie de dibujos realizada entre el 2010 y 2014 que ilustra irónicamente el amor y el  fracaso.",
+        },
+        img: "img/galeria/cuadernos_amor_2.jpg",
+    },
+    {
+        id: "opuntita",
+        texto: {
+            titulo: "Opuntia engelmannii var. aciculata",
+            tecnica: "Mixta; acuarela en pasta con lápices suaves",
+            material: "Papel Canson Mix media 300g-  22.9 cm x 30.5 cm",
+            anyo: "2020",
+            autor: "Eugenia Alaniz Fierro",
+            acerca_obra: " Esta obra inicialmente fue pensada para la identificación de un ejemplar de O. engelmannii, sin embargo, durante su observación. Se determino que por aspectos morfológicos no era la especie con la que se trabajaba, si no una variedad de la misma especie, el detalle de las espinas fue un elemento crucial para su identificación, siendo las técnicas empleadas en la ilustración lo que permitió darle un toque especial a la ilustración como su apreciación. Esto dio pie a volverse una obra más de una serie de ilustraciones científicas sobre cactaceas.",
+        },
+        img: "img/galeria/opuntita.jpeg",
+    },
+    {
         id: "--",
         texto: {
             titulo: "",
@@ -20,6 +54,17 @@ const Catalogo = [
             acerca_obra: "",
         },
         img: "img/galeria/",
+    },
+    {
+        id: "lado_b",
+        texto: {
+            titulo: "Lado B del Lado B",
+            tecnica: "Videoarte",
+            anyo: "2021",
+            autor: "Pablo Aguirre",
+            acerca_obra: "La naturaleza efímera y anónima de las comunicaciones en línea promueve una apariencia de libertad similar a las conversaciones privadas. Sin embargo, esta libertad también propicia la propagación del error y la desinformación. A través de rumores, teorías conspirativas y emociones desbordadas expresadas sin filtro ni verificación, el error y la información falsa se cuelan y consolidan momentáneamente en el espacio público virtual. Este trabajo analiza conversaciones de WhatsApp durante la pandemia para desentrañar las estructuras conceptuales inconsistentes y llenas de contradicciones que subyacen a las fracturas sociopolíticas, y la materialidad fértil pero también problemática que encierran los impulsos irracionales expresados en una época de incertidumbre. Todo ello a través de un \"hackeo estadístico\" que problematiza la inestable pero reveladora arquitectura del error y el sinsentido en las comunicaciones digitales. \n\n\n\nPresentado en el Centro Cultural de España en La Paz-Bolivia. Dentro de la exposición colectiva \"Mutaciones y afecciones #cuerpospandemicos\". ",
+        },
+        video: "img/galeria/lado_b.mp4",
     },
 ]
 
