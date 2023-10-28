a5fbc263c2bd58855582b760da937ccd6d8a8669
Azxma <bluakorvo@protonmail.com>
2023-10-28 17:26:58 -0600

    [MODIFICA] el index para indicar cuadno es un video por medio de la base de datos

--- js/programa.js
+++ js/programa.js
@@ -18,7 +18,8 @@ function clickeado(...argumentos){
         imagen_lateral.classList.add("imagen_lateral_anim")
         document.body.style.overflow="hidden"
 
-        let entrada_encontrada = Catalogo.find((entrada) => entrada.id == argumentos[1]) ?? {texto: "Parece que no tenemos ese dato", img: "https://i.redd.it/floofy-sleep-time-hololive-v0-3oxipphrpjtb1.jpg?s=a8c4a0940c5ce6c2210bcc08baacca61b253fd06"}
+        let entrada_encontrada = Catalogo.find((entrada) => entrada.id == argumentos[1]) ?? 
+            {texto: "Parece que no tenemos ese dato", img: "https://i.redd.it/floofy-sleep-time-hololive-v0-3oxipphrpjtb1.jpg?s=a8c4a0940c5ce6c2210bcc08baacca61b253fd06"}
 
         texto_lateral.querySelector("#titulo_place").innerText = entrada_encontrada.texto.titulo ?? "Erro 404"
         texto_lateral.querySelector("#anyo_place").innerText = entrada_encontrada.texto.anyo ?? ""
@@ -41,9 +42,7 @@ function clickeado(...argumentos){
             texto_lateral.querySelector("#material").classList.add("display_hide")
         }
 
-        if(argumentos[2] == 'video'){
-            console.log("video")
-
+        if(entrada_encontrada.video){
             imagen_lateral.querySelector("img").style.display = 'none'
             imagen_lateral.querySelector("video").style.display = 'flex'
             imagen_lateral.querySelector("video").src = entrada_encontrada.video
@@ -57,7 +56,7 @@ function clickeado(...argumentos){
 
         }
         
-        imagen_lateral.querySelector("img").src = entrada_encontrada.img
+        imagen_lateral.querySelector("img").src = entrada_encontrada.img ?? ""
 
         estado_actual = estados_posibles.visisble
     }
